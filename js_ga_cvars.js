ea2db738a791834d669b4405bb8ea74ceebfb0ff
Val Lyashov <val@plstr.com>
2012-11-05 15:42:51 +1100

    v.4
    Rewrote the slot 1 storage, including additional code from Urchin.js to
    grab current slot key, to use for appending additional user types to.

--- js_ga_cvars.js
+++ js_ga_cvars.js
@@ -1,19 +1,3 @@
-/*
-  slot 1: ['type1','type2','type3']
-  slot 2: [33, 12, 4]
-  slot 3: ['s33333', '093333', ['nx9123','nx891283']]
-  slot 4: //touchpoints
-  slot 5: {'1':1, '2':3}
-
-Notes:
-  - slot 1 always sorted
-  - slot 2 follows slot 1 positions
-  - some milestones can have multiple areas that can increment all values
-    e.g. accommodation, life on campus etc.
-  - slot 5 increments even if already exists, though slot 2 doesn't
-  - slot 3 follows slot1 layout and goes 2d if entry already exists.
-*/
-
 
 // IE fix from the following:
 // https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/indexOf
@@ -25,138 +9,124 @@ d=(d>0||-1)*Math.floor(Math.abs(d))}}if(d>=c){return-1}
 var e=d>=0?d:Math.max(c-Math.abs(d),0);for(;e<c;e++){
   if(e in b&&b[e]===a){return e}}return-1}}
 
-//constants
+// cookie process function from urchin.js, thanks Google.
+function _uGC(a,b,c){if(!a||a==""||!b||b==""||!c||c=="")return"-";
+var d,e,f,g="-";d=a.indexOf(b);f=b.indexOf("=")+1;if(d>-1){e=a.indexOf(c,d);
+if(e<0){e=a.length}g=a.substring(d+f,e)}return g};
+
+
+// constants
 GA_NAME = 'inj';
 ERROR = -1;
 
+// grab cookie with customvars
+function retCookie()
+{
+    var c = unescape(_uGC(document.cookie, '__utmv=', ';'));
+    c = _uGC(c, '1=', '|').split('^')[0].split('=')[0]
+    return (c == '-') ? "" : c;
+};
+
 // helper blocks
-function getLoc(data, string)
-{
-  data.sort();
-  return data.indexOf(string);
-};
-
-function insertItem(data, item)
-{
-  data.push(item);
-  data.sort();
-  return data.indexOf(item);
-};
-
-function parseSlot(slot)
-{
-  var current = _gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(slot);
-  if(current)
-  {
-    current = current.split(', ');
-    return current;
-  }
-  return 0;
-};
-
-function parseDict(slot)
-{
-  var current = _gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(slot);
-  if(current)
-  {
-    current = JSON.parse(_gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(slot));
-    return current;
-  }
-  return 0;
-};
-
 function clearAll()
 {
-  for(var i=0; i<=5;i++)
-    _gaq.push(['inj._deleteCustomVar', i]);
-  console.log('All custom vars cleared.');
+    for(var i=0; i<=5;i++)
+        _gaq.push(['inj._deleteCustomVar', i]);
+    console.log('All custom vars cleared.');
 };
 
 function printAllVars()
 {
-  console.log('GA custom vars:')
-  for(var i=1; i<=5;i++)
-    console.log(i+ ': ' + _gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(i));
+    console.log('GA custom vars:')
+    console.log("Slot 1 key: " + retCookie());
+    for(var i=1; i<=5;i++)
+        console.log(i+ ': ' + _gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(i));
 };
 
-
-// Set demographic information.
-function addType(user_type)
+function getLoc(data, string)
 {
-  var current = parseSlot(1) || [];
-  var loc;
-  if(current.length > 0)
-    { 
-        if(current.indexOf(user_type) != ERROR)
-          return ERROR;
-    }  
+    data.sort();
+    return data.indexOf(string);
+};
 
-    loc = insertItem(current, user_type);
-    _gaq.push(['inj._setCustomVar',1,current.join(', '),current.join(', '),1]);
+function insertItem(data, item)
+{
+    data.push(item);
+    data.sort();
+    return data.indexOf(item);
+};
+
+function parseSlot(slot)
+{
+    var current = _gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(slot);
+    if(current)
+    {
+        current = current.split(', ');
+        return current;
+    }
+    return 0;
+};
+
+function parseDict(slot)
+{
+    var current = _gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(slot);
+    if(current)
+    {
+        current = JSON.parse(_gat._getTrackerByName(GA_NAME)._getVisitorCustomVar(slot));
+        return current;
+    }
+    return 0;
+};
+
+/************************************
+ * main functions
+ ***********************************/
+// Slot 1: Set demographic information + points
+function addType(user_type, page_value)
+{
+    var c = retCookie();
+    var current = (c) ? c.split(', ') : [];
+    var loc;
+    var scores;
+    loc = (current.indexOf(user_type) == ERROR) ? insertItem(current, user_type) : ERROR;
+    scores = (loc == ERROR) ? addScore(0, page_value, 0) : addScore(loc, page_value, 1);
+    _gaq.push(['inj._setCustomVar', 1, current.join(', '), scores, 1]);
     return loc;
 };
 
-// Increment engagement score
+// Slot 1: Increment engagement score
 function addScore(loc, new_value, isNew)
 {
-  var current = parseSlot(2) || [];
-  if(isNew)
-  {
-    current.splice(loc, 0, new_value);
-  }
-  else
-  {
-    if(current.length != 0 && current.length >= loc)
-        current[loc] += new_value;
-      else  
-        current.push(new_value);
-  }
-    _gaq.push(['inj._setCustomVar',2,current.join(', '),current.join(', '),1]);
-    return 1;
+    var current = parseSlot(1) || [];
+    if(isNew)
+        current.splice(loc, 0, new_value);
+    else
+    {
+        if(current.length != 0 && current.length >= loc)
+            current[loc] = parseInt(current[loc]) + new_value;
+        else 
+            current.push(new_value);
+    }
+    return current.join(', ');
 };
 
-// add Nexis/ISIS ID for specific demographic
-function addID(loc, new_id)
-{
-  var current = parseSlot(3) || [];
-  var temp;
-  if(current.length >= loc)
-  {
-    if(current[loc].indexOf(new_id) != ERROR)
-      return ERROR;
-
-    if(typeof(current[loc]) == 'string')
-      {
-        temp = current[loc];
-        current[loc] = [temp, new_id];
-      }
-      else
-        current[loc].push(new_id);
-  }
-    else  
-      current.push(new_value);
-    _gaq.push(['inj._setCustomVar',3,current.join(', '),current.join(', '),1]);
-    return 1;
-};
-
+// Slot 5: add page to visited list
 function addVisit(page_id)
 {
-  var current = parseDict(5) || {};
-  var confirm = 1;
-  if(current[page_id])
-  {
-    current[page_id] += 1;
-    confirm = ERROR;
-  }
-  else 
-      current[page_id] = 1;
-
+    var current = parseDict(5) || {};
+    var confirm = 1;
+    if(current[page_id])
+    {
+        current[page_id] += 1;
+        confirm = ERROR;
+    }
+    else 
+        current[page_id] = 1;
     _gaq.push(['inj._setCustomVar',5,JSON.stringify(current),JSON.stringify(current),1]);
     return confirm;
 };
 
-
-// run through the actions
+// Init: run through the actions
 function pushMilestone(demographic, page_value, page_id)
 {
     var check = ERROR;
@@ -166,51 +136,7 @@ function pushMilestone(demographic, page_value, page_id)
     check = addVisit(page_id);
 
     if(check != ERROR)
-    {
-        loc = addType(demographic);
-        console.log(loc);
-        if(loc == ERROR)
-          isNew = 0;
-        addScore(loc, page_value, isNew);
-    }
+        addType(demographic, page_value);
     _gaq.push(['inj._trackEvent','user activity', 'status change',]);
     return check;
 };
-
-/********************************************************************
- * Testing bit:
- *    - Chrome (win)
- *    - IE 9
- *    - Firefox (win)
- ********************************************************************/
-
-// clear all custom vars for testing
-clearAll();
-
-/* Running sample: usertype 1 */
-
-var demographic = 'type1';
-var page_value = 4;
-var page_id = 1;
-
-try 
-{
-  (pushMilestone(demographic, page_value, page_id) == ERROR) ?
-    console.log('Something went horribly wrong.') : console.log('Success.');
-}
-catch (error) 
-{
-  console.log('Caught: ' + error.message);
-}
-
-
-/* Running sample: usertype 2 */
-
-var demographic = 'type2';
-var page_value = 6;
-var page_id = 12;
-
-// run data push
-(pushMilestone(demographic, page_value, page_id) == ERROR) ?
-  console.log('Something went horribly wrong.') : console.log('Success.');
-
